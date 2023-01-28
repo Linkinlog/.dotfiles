8f38a00e199f7e482aa0da3fff4a1df6d931af40
Linkinlog <dahlton@linkd.dev>
2023-01-27 22:45:47 -0500

    Fix: Windows support

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,10 @@
-Path = os.getenv("HOME") .. "/.config/wezterm/space-astronaut.jpg"
+-- Path env var is HOME on unix and HOMEPATH on windows
+local home = os.getenv( "HOME" ) or os.getenv( "HOMEPATH" )
+local fullPath = home .. "/.config/wezterm/space-astronaut.jpg"
 return {
     background = {
         {
-            source = { File = Path, },
+            source = { File = fullPath, },
             hsb = { brightness = 0.07 },
             repeat_x = "NoRepeat",
             repeat_y = "NoRepeat",
