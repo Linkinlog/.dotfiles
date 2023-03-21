e4e8f49362e48803aab3859ea20d42a6df9a3242
Linkinlog <dahlton@dahlton.org>
2023-03-20 21:11:50 -0400

    Feat: more nix config

--- .config/nix/default.nix
+++ .config/nix/default.nix
@@ -2,14 +2,13 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/22.11.tar.gz") {};
 in {
   
-  git = pkgs.git;
-  neovim = pkgs.neovim;
-  gh = pkgs.gh;
+  git     = pkgs.git;
+  neovim  = pkgs.neovim;
+  gh      = pkgs.gh;
   lazygit = pkgs.lazygit;
   
-  go = pkgs.go;
-  rustc = pkgs.rustc;
-  cargo = pkgs.cargo;
-  bash = pkgs.bash;
-  zsh = pkgs.zsh;
+  go      = pkgs.go;
+  rustup  = pkgs.rustup;
+  bash    = pkgs.bash;
+  zsh     = pkgs.zsh;
 }
--- .zshrc
+++ .zshrc
@@ -1,1 +1,3 @@
 source ~/.config/zsh/.zshrc
+
+if [ -e /home/log/.nix-profile/etc/profile.d/nix.sh ]; then . /home/log/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
