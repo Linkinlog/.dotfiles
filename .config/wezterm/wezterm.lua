c5919fa9621876b5bbd8295b7fcb997e4639b0dc
Linkinlog <dahlton@dahlton.org>
2023-03-21 19:54:35 -0400

    Fix: wezterm gpu issues

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,11 @@
 -- Path env var is HOME on unix and HOMEPATH on windows
 local home = os.getenv( "HOME" ) or os.getenv( "HOMEPATH" )
 local fullPath = home .. "/.config/wezterm/space-astronaut.jpg"
+local wezterm = require 'wezterm'
+local gpus = wezterm.gui.enumerate_gpus()
 return {
+    webgpu_preferred_adapter = gpus[2],
+    front_end = 'WebGpu',
     background = {
         {
             source = { File = fullPath, },
