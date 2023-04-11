8e266368c97de01973d47601c63367411b744522
Linkinlog <dahlton@dahlton.org>
2023-04-11 11:27:08 -0400

    Feat: New Backgrounds

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,13 @@
+-- Pick an image
+local image = "solen-feyissa-aQHilAxwPHk-unsplash.jpg"
 -- Path env var is HOME on unix and HOMEPATH on windows
 local home = os.getenv( "HOME" ) or os.getenv( "HOMEPATH" )
-local fullPath = home .. "/.config/wezterm/space-astronaut.jpg"
-local wezterm = require 'wezterm'
-local gpus = wezterm.gui.enumerate_gpus()
+local fullPath = home .. "/.config/wezterm/backgrounds/" .. image
 return {
-    webgpu_preferred_adapter = gpus[2],
-    front_end = 'WebGpu',
     background = {
         {
             source = { File = fullPath, },
-            hsb = { brightness = 0.07 },
+            hsb = { brightness = 0.01 },
             repeat_x = "NoRepeat",
             repeat_y = "NoRepeat",
             height = "Cover",
