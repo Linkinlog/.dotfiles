3b1aed3b3455773a75b4c99289e33b83a86ee4fd
Linkinlog <dahlton@dahlton.org>
2023-04-13 20:54:55 -0400

    Feat: various

--- .local/bin/extract_media.sh
+++ .local/bin/extract_media.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Check if unrar is installed
-if ! command -v unrar >/dev/null 2>&1; then
-  printf "\033[31mError:\033[0m 'unrar' is not installed. Please install it and try again.\n"
-  exit 1
-fi
-
 # ANSI escape sequences for colors
 GREEN="\033[32m"
 RED="\033[31m"
@@ -19,47 +13,44 @@ CROSS_MARK="\xE2\x9D\x8C"
 ROCKET="\xF0\x9F\x9A\x80"
 FIRE="\xF0\x9F\x94\xA5"
 
-verbose=false
-while getopts ":v" option; do
-  case "$option" in
-    v)
-      verbose=true
-      ;;
-    *)
-      printf "Usage: %s [-v] /path/to/download/directory\n" "$0"
-      exit 1
-      ;;
-  esac
+# Check if unrar is installed
+if ! command -v unrar >/dev/null 2>&1; then
+  echo -e "${RED}Error:${RESET} 'unrar' is not installed ${CROSS_MARK}. Please install it and try again ${FIRE}.\n"
+  exit 1
+fi
+
+# Check for environment variables and use them first
+if [ -n "$sonarr_episodefile_sourcefolder" ]; then
+  download_path="$sonarr_episodefile_sourcefolder"
+elif [ -n "$radarr_moviefile_sourcefolder" ]; then
+  download_path="$radarr_moviefile_sourcefolder"
+elif [ -n "$1" ]; then
+  download_path="$1"
+else
+  printf "Usage: %s /path/to/rar_archive/directory\n" "$0"
+  exit 1
+fi
+
+# Use a for loop to process the output of the find command
+mapfile -d '' archive_files < <(find "$download_path" -type f -iname "*.rar" -print0)
+
+for archive_path in "${archive_files[@]}"; do
+  output_dir="$(dirname "$archive_path")"
+  log_file="${output_dir}/archive_extraction_$(date +"%Y%m%d").log"
+
+  separator="${YELLOW}$(printf '*%.0s' {1..50})${RESET}"
+  log_entry="\n$separator\n$CYAN$ROCKET Starting extraction process on: $(date) $ROCKET$RESET\n$separator"
+  echo -e "$log_entry" | tee -a "$log_file"
+
+  echo -e "${CYAN}Extracting: $archive_path $RESET" | tee -a "$log_file"
+
+  if unrar x -y "$archive_path" "$output_dir" >> "$log_file" 2>&1; then
+    success_entry="${GREEN}Extraction successful: $archive_path $CHECK_MARK$RESET\n$separator"
+    echo -e "$success_entry" | tee -a "$log_file"
+  else
+    failure_entry="${RED}Extraction failed: $archive_path $CROSS_MARK$RESET\n$separator"
+    echo -e "$failure_entry" | tee -a "$log_file"
+  fi
+  finish_entry="${CYAN}$FIRE Extraction process finished on: $(date) $FIRE$RESET"
+  echo -e "$finish_entry" | tee -a "$log_file"
 done
-shift $((OPTIND-1))
-
-download_path="$1"
-
-find "$download_path" -type f -iname "*.rar" -print0 \
-  | while IFS= read -r -d '' archive_path; do
-      output_dir="$(dirname "$archive_path")"
-      log_file="${output_dir}/archive_extraction_$(date +"%Y%m%d").log"
-      
-      separator="${YELLOW}$(printf '*%.0s' {1..50})${RESET}"
-      echo -e "\n$separator\n$CYAN$ROCKET Starting extraction process on: $(date) $ROCKET$RESET\n$separator" | tee -a "$log_file"
-      
-      if $verbose; then
-        echo -e "${CYAN}Extracting: $archive_path $RESET"
-      fi
-      echo -e "${CYAN}Extracting: $archive_path $RESET" >> "$log_file"
-
-      if unrar x -y "$archive_path" "$output_dir" >> "$log_file" 2>&1; then
-        if $verbose; then
-          echo -e "${GREEN}Extraction successful: $archive_path $CHECK_MARK$RESET$CYAN\n$separator $RESET"
-        fi
-        echo -e "${GREEN}Extraction successful: $archive_path $CHECK_MARK$RESET\n$separator" >> "$log_file"
-      else
-        if $verbose; then
-          echo -e "${RED}Extraction failed: $archive_path $CROSS_MARK$RESET$CYAN\n$separator $RESET"
-        fi
-        echo -e "${RED}Extraction failed: $archive_path $CROSS_MARK$RESET\n$separator" >> "$log_file"
-      fi
-
-      echo -e "${CYAN}$FIRE Extraction process finished on: $(date) $FIRE$RESET"
-      echo -e "$separator" | tee -a "$log_file"
-    done
