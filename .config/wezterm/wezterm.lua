4b9d9fd33550b89cdc4df45bae181ed3018e0453
Linkinlog <dahlton@dahlton.org>
2023-06-01 12:04:22 -0400

    feat(wezterm): Update config to new style

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,15 +3,36 @@ local image = "solen-feyissa-aQHilAxwPHk-unsplash.jpg"
 -- Path env var is HOME on unix and HOMEPATH on windows
 local home = os.getenv( "HOME" ) or os.getenv( "HOMEPATH" )
 local fullPath = home .. "/.config/wezterm/backgrounds/" .. image
-return {
-    background = {
-        {
-            source = { File = fullPath, },
-            hsb = { brightness = 0.01 },
-            repeat_x = "NoRepeat",
-            repeat_y = "NoRepeat",
-            height = "Cover",
-            horizontal_align = "Right",
-        }
-    },
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font_with_fallback {
+  'JetBrains Mono',
+  'Fira Mono',
 }
+
+config.background = {
+    {
+        source = { File = fullPath, },
+        hsb = {
+            brightness = 0.02,
+            hue = 1.2,
+            saturation = 1.3,
+        },
+        repeat_x = "NoRepeat",
+        repeat_y = "NoRepeat",
+        height = "Cover",
+        horizontal_align = "Right",
+    }
+}
+
+return config
